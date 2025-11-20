171584e564f6d2cd06fd2f4291582137a420a61f
KIRISHIKI Yudai <yudai.kirishiki@protonmail.com>
2025-11-20 13:09:58 +0900

    refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod transport;
 
 pub use clf::{errors::*, targets::*};
 pub use driver::port100;
-pub use driver::port100::driver::{Chipset, Device, init, open_port100_device};
+pub use driver::port100::driver::{Chipset, Device, init as init_port100, open_port100_device};
 pub use driver::port400;
 pub use driver::port400::driver::{
     Device as Port400Device, init as init_port400, open_port400_device,
